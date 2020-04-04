684fd8eb445635df0a92281eb58b8bbfdbdd73a4
Ahmet Alp Balkan <ahmetb@google.com>
2020-04-04 14:14:48 -0700

    initial commit
    Signed-off-by: Ahmet Alp Balkan <ahmetb@google.com>

---
+++ go.mod
@@ -0,0 +1,8 @@
+module kcat
+
+go 1.14
+
+require (
+	github.com/fatih/color v1.9.0
+	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c
+)
---
+++ main.go
@@ -0,0 +1,75 @@
+// Copyright 2020 Google Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//     http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"regexp"
+
+	"github.com/fatih/color"
+	"gopkg.in/yaml.v3"
+)
+
+func main() {
+	file := os.Args[1]
+	b, err := ioutil.ReadFile(file)
+	if err != nil {
+		panic(err)
+	}
+
+	// decode
+	var v yaml.Node
+	if err := yaml.Unmarshal(b, &v); err != nil {
+		panic(err)
+	}
+	// TODO investigate how are multiple docs coming to Node
+	if len(v.Content) == 0 {
+		panic("no yaml docs found")
+	}
+
+	// TODO support multiple documents
+	content := v.Content[0]
+
+	colorizeKeys(content)
+
+	var buf bytes.Buffer
+	enc := yaml.NewEncoder(&buf)
+	enc.SetIndent(2)
+	enc.Encode(content)
+
+	fmt.Print(render(buf))
+}
+
+func render(buf bytes.Buffer) string {
+	s := buf.String()
+
+	re := regexp.MustCompile(`(?m)(KEY_)([^:]+)`)
+
+	return re.ReplaceAllString(s,  color.RedString(`$2$3`))
+}
+
+func colorizeKeys(node *yaml.Node) {
+	for i, child := range node.Content {
+		if node.Kind == yaml.SequenceNode && child.Kind == yaml.ScalarNode {
+			continue
+		}
+		// key'ler 2'ye bolunuyor
+		if i%2 == 0 && child.Value != "" {
+			child.Value = "KEY_" + child.Value
+		}
+		colorizeKeys(child)
+	}
+}
